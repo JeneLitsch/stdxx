23ed8b0ea8385ea3a3ece98146f48306e32dbeff
Jene Litsch <litsch.jene@gmail.com>
2022-01-04 14:25:40 +0100

    concat.hxx -> array.hxx

--- stdxx/array.hxx
+++ stdxx/array.hxx
@@ -1,9 +1,10 @@
 #pragma once
 #include <array>
 #include <vector>
+#include <span>
 
-template<class T, std::size_t SIZE_L, std::size_t SIZE_R>
-std::array<T, SIZE_L + SIZE_R> operator+(
+template<typename T, std::size_t SIZE_L, std::size_t SIZE_R>
+inline std::array<T, SIZE_L + SIZE_R> operator+(
 	const std::array<T, SIZE_L> & l,
 	const std::array<T, SIZE_R> & r) {
 	std::array<sf::Vertex, SIZE_L + SIZE_R> arr;
@@ -12,11 +13,20 @@ std::array<T, SIZE_L + SIZE_R> operator+(
 	return arr;
 }
 
-template<class T, std::size_t SIZE>
-std::vector<T> & operator+=(
+template<typename T, std::size_t SIZE>
+inline std::vector<T> & operator+=(
 	std::vector<T> & vec,
 	const std::array<T, SIZE> & arr) {
 	vec.reserve(vec.size() + SIZE);
 	std::copy_n(arr.begin(), SIZE, std::back_inserter(vec));
 	return vec;
+}
+
+namespace stx {
+	template<typename T>
+	inline void append(std::vector<T> & vec, const auto & elems) {
+		for(const auto & elem : elems) {
+			vec.push_back(elem);
+		}
+	}
 }
