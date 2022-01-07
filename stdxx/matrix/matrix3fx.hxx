34f839552186fc0b82a80abd02219a0efe739109
Jene Litsch <litsch.jene@gmail.com>
2022-01-07 22:17:30 +0100

    Fixed matrix inverse

--- stdxx/matrix/matrix3fx.hxx
+++ stdxx/matrix/matrix3fx.hxx
@@ -4,9 +4,9 @@
 namespace stx {
 	template<typename T>
 	constexpr inline T det(const matrix3<T> & m) {
-		return m(0, 0) * (m(1, 1) * m(2, 2) - m(2, 1) * m(1, 2)) 
-		     - m(0, 1) * (m(1, 0) * m(2, 2) - m(1, 2) * m(2, 0)) 
-	         + m(0, 2) * (m(1, 0) * m(2, 1) - m(1, 1) * m(2, 0));
+		return m(0,0) * (m(1,1) * m(2,2) - m(1,2) * m(2,1))
+		     - m(1,0) * (m(0,1) * m(2,2) - m(2,1) * m(0,2))
+			 + m(2,0) * (m(0,1) * m(1,2) - m(1,1) * m(0,2));
 	}
 
 	template<typename T>
@@ -14,14 +14,14 @@ namespace stx {
 		const auto det = stx::det(m);
 		const auto invdet = 1 / det;
 		return stx::matrix3<T> {
-			(m(1, 1) * m(2, 2) - m(2, 1) * m(1, 2)) * invdet,
-			(m(0, 2) * m(2, 1) - m(0, 1) * m(2, 2)) * invdet,
-			(m(0, 1) * m(1, 2) - m(0, 2) * m(1, 1)) * invdet,
-			(m(1, 2) * m(2, 0) - m(1, 0) * m(2, 2)) * invdet,
-			(m(0, 0) * m(2, 2) - m(0, 2) * m(2, 0)) * invdet,
-			(m(1, 0) * m(0, 2) - m(0, 0) * m(1, 2)) * invdet,
-			(m(1, 0) * m(2, 1) - m(2, 0) * m(1, 1)) * invdet,
-			(m(2, 0) * m(0, 1) - m(0, 0) * m(2, 1)) * invdet,
-			(m(0, 0) * m(1, 1) - m(1, 0) * m(0, 1)) * invdet};
+			(m(1,1) * m(2,2) - m(1,2) * m(2,1)) * invdet,
+			(m(2,0) * m(1,2) - m(1,0) * m(2,2)) * invdet,
+			(m(1,0) * m(2,1) - m(2,0) * m(1,1)) * invdet,
+			(m(2,1) * m(0,2) - m(0,1) * m(2,2)) * invdet,
+			(m(0,0) * m(2,2) - m(2,0) * m(0,2)) * invdet,
+			(m(0,1) * m(2,0) - m(0,0) * m(2,1)) * invdet,
+			(m(0,1) * m(1,2) - m(0,2) * m(1,1)) * invdet,
+			(m(0,2) * m(1,0) - m(0,0) * m(1,2)) * invdet,
+			(m(0,0) * m(1,1) - m(0,1) * m(1,0)) * invdet};
 	}
 }
