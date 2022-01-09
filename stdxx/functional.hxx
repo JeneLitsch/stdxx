c149b24560e2a43b83c487be8d6f766b77e42ecf
Jene Litsch <litsch.jene@gmail.com>
2022-01-09 22:07:53 +0100

    functional.hxx

---
+++ stdxx/functional.hxx
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace stx {
+	constexpr auto deref(auto & ptr_like) {
+		return *ptr_like;
+	}
+
+	constexpr auto before(auto main_fx, auto called_before) {
+		return [main_fx, called_before] (auto & param) {
+			return main_fx(called_before(param));
+		};
+	}
+
+	// For predicate functions: Derefs the single parameter
+	// Allows to use algorithms on containers with pointer types.
+	// e.g. std::vector<std::unique_ptr<?>>
+	constexpr auto deref_parameter(auto fx) {
+		return [fx] (const auto & ptr) { return fx(*ptr); };
+	}
+}
