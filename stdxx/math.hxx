5a28b04e5575eb2c1a9c04428a2f47ccefd9fdd9
Jene Litsch <litsch.jene@gmail.com>
2022-04-10 11:44:08 +0200

    stx::wrap

--- stdxx/math.hxx
+++ stdxx/math.hxx
@@ -8,4 +8,16 @@ namespace stx {
 		const auto e = std::numbers::e_v<decltype(x)>;
 		return 1 / (1 + std::pow(e, -x)); 
 	}
+
+	constexpr auto mod(std::floating_point auto x, std::floating_point auto y) {
+		return std::fmod(x, y);
+	}
+
+	constexpr auto mod(std::integral auto x, std::integral auto y) {
+		return x % y;
+	}
+
+	constexpr auto wrap(auto x, auto y) {
+		return  mod(mod(x, y) + y, y);
+	}
 }
