67801f5148762069a48a3fd39a39ca71bab03c4b
Jene Litsch <litsch.jene@gmail.com>
2022-05-07 11:59:11 +0200

    Fixed array +

--- stdxx/array.hxx
+++ stdxx/array.hxx
@@ -7,7 +7,7 @@ template<typename T, std::size_t SIZE_L, std::size_t SIZE_R>
 constexpr inline std::array<T, SIZE_L + SIZE_R> operator+(
 	const std::array<T, SIZE_L> & l,
 	const std::array<T, SIZE_R> & r) {
-	std::array<sf::Vertex, SIZE_L + SIZE_R> arr;
+	std::array<T, SIZE_L + SIZE_R> arr;
 	std::copy_n(l.begin(), SIZE_L, arr.begin());
 	std::copy_n(r.begin(), SIZE_R, arr.begin() + SIZE_L);
 	return arr;
