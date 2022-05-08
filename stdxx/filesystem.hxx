5e4cb992da28472c4f5762af7faef9de7ac5a84b
Jene Litsch <litsch.jene@gmail.com>
2022-05-08 11:14:32 +0200

    fixed stx::fs::replace_stem

--- stdxx/filesystem.hxx
+++ stdxx/filesystem.hxx
@@ -12,9 +12,7 @@ namespace stx::filesystem::literals {
 namespace stx::fs {
 	inline auto replace_stem(std::filesystem::path path, auto new_root) {
 		const auto extension = path.extension();
-		return path.
-			replace_filename(new_root).
-			replace_extension(extension.string());
+		return path.replace_filename(new_root).concat(extension.string());
 	}
 
 	inline auto concat_stem(std::filesystem::path path, auto str) {
