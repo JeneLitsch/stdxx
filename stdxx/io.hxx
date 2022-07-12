6675ecda6f0e6f6e273d77dc98ebaadd1672f214
Jene Litsch <litsch.jene@gmail.com>
2022-07-13 00:38:14 +0200

    stx::separated

--- stdxx/io.hxx
+++ stdxx/io.hxx
@@ -9,22 +9,28 @@
 #include "iterator.hxx"
 
 namespace stx {
+	
 	inline void print(std::ostream & stream, auto data) {
 		stream << data;
 	}
+
+
 
 	inline void print(auto data) {
 		print(std::cout, data);
 	}
 
+
+
 	inline void println(std::ostream & stream, auto data) {
 		stream << data << "\n";
 	}
 
+
+
 	inline void println(auto data) {
 		println(std::cout, data);
 	}
-
 
 
 	
@@ -35,12 +41,16 @@ namespace stx {
 		return value;
 	}
 	
+
+
 	template<vector_2 T>
 	inline T read(std::istream & in) {
 		const auto x = read<decltype(T::x)>(in);
 		const auto y = read<decltype(T::y)>(in);
 		return T{x, y};
 	}
+
+
 
 	template<vector_3 T>
 	inline T read(std::istream & in) {
@@ -50,34 +60,47 @@ namespace stx {
 		return T{x, y, z};
 	}
 
+
+
 	template<typename...T>
 	inline auto read() {
 		return read<T...>(std::cin);
 	}
 
 	
+
 	inline std::string readln(std::istream & stream) {
 		std::string str;
 		std::getline(stream, str);
 		return str;
 	}
 	
+
+
 	inline std::string readln() {
 		return readln(std::cin);
 	}
+
+
 
 	inline bool isEof(std::istream & stream) {
 		return stream.eof();
 	}
 
+
+
 	inline bool isEof() {
 		return isEof(std::cin);
 	}
+
+
 
 	template<typename T>
 	inline std::tuple<T> read_n(std::istream & in) {
 		return std::make_tuple(read<T>(in));
 	}
+
+
 
 	template<typename T1, typename T2, typename... Ts>
 	inline auto read(std::istream & in) {
@@ -86,12 +109,29 @@ namespace stx {
 		return std::tuple_cat(tuple1, tuple2);
 	}
 
+
+
 	std::string quote(const auto & printable) {
 		std::stringstream ss;
 		ss << "\"" << printable << "\"";
 		return ss.str();
 	}
 
+
+
+	void separated(auto begin, auto end, std::ostream & out, const char * padding) {
+		const char * p = "";
+		std::for_each(begin, end, [&] (auto elem) {
+			out << p << elem;
+			p = padding; 
+		});
+	}
+
+
+
+	void separated(auto begin, auto end, std::ostream & out, std::string_view padding) {
+		padded(begin, end, out, padding.data());
+	}
 }
 
 std::ostream & operator<<(std::ostream & out, const stx::color_rgb auto & color) {
