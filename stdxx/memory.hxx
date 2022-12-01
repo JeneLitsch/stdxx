b5f9854288da41e1157aea4a149fd5b983e0c83d
Jene Litsch <litsch.jene@gmail.com>
2022-12-01 17:36:01 +0100

    Improved stx::make_unique

--- stdxx/memory.hxx
+++ stdxx/memory.hxx
@@ -1,9 +1,15 @@
+#pragma once
 #include <memory>
 
 namespace stx {
 	template<typename T, typename ... Args>
 	std::unique_ptr<T> make_unique(Args && ... args) {
-		return std::unique_ptr<T>(new T{std::forward<Args>(args)...});
+		if constexpr (std::is_constructible_v<T, Args...>) {
+        	return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+		}
+		else {
+			return std::unique_ptr<T>(new T{std::forward<Args>(args)...});
+		}
 	}
 
 
