a900b44421cb31108d80b987cb148e8aa6d33a14
Jene Litsch <litsch.jene@gmail.com>
2022-12-08 20:06:35 +0100

    stx::fx::map and stx::mapped::map are now constexpr

--- stdxx/functional/map.hxx
+++ stdxx/functional/map.hxx
@@ -3,14 +3,14 @@
 
 namespace stx::fx {
 	template<typename T, typename ... Args>
-	auto map(auto && f, const std::vector<T> & vec, Args && ... args) {
+	constexpr auto map(auto && f, const std::vector<T> & vec, Args && ... args) {
 		using R = decltype(f(std::declval<T>(), (std::declval<Args>())...));
 		std::vector<R> results;
 		for(const auto & elem : vec) results.push_back(f(elem, args...));
 		return results;
 	}
 
-	auto mapped(auto && f) {
+	constexpr auto mapped(auto && f) {
 		return [f] (const auto & collection, auto && ... args) {
 			return map(f, collection, args...); 
 		};
