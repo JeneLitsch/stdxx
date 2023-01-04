329c302a2ab805b6a4fdf69e63eef71b7252c08d
Jene Litsch <litsch.jene@gmail.com>
2023-01-04 09:47:28 +0100

    Minor tweeking of stx::lerp and updated stx::lerp documentation

--- doc/math/lerp.md
+++ doc/math/lerp.md
@@ -20,7 +20,9 @@ Lerp function(s) compatible with different types
 | 3   | Floating  | lerp(Floating, Floating, Floating)   |
 | 4   | Vec2      | lerp(Vec2, Vec2, Floating)           |
 | 5   | Vec3      | lerp(Vec3, Vec3, Floating)           |
+| 6   | RGB       | lerp(RGB, RGB, Floating)             |
+| 7   | RGBA      | lerp(RGBA, RGBA, Floating)           |
 
 1 - 3) Simple numeric linear interpolation
 
-4 - 5) Elementwise linear interpolation. A lerp is performed along each axis.
+4 - 7) Elementwise linear interpolation. A lerp is performed along each axis.
--- stdxx/math/lerp.hxx
+++ stdxx/math/lerp.hxx
@@ -11,6 +11,7 @@ namespace stx {
 	}
 
 
+
 	template<std::unsigned_integral U, std::floating_point T>
 	U lerp(U a, U b, T t) {		
 		using X = std::common_type_t<U, T>;
@@ -39,7 +40,7 @@ namespace stx {
 
 
 
-	template<vector_3 Vec3 , std::floating_point T>
+	template<vector_3 Vec3, std::floating_point T>
 	Vec3 lerp(const Vec3 & a, const Vec3 & b, T t) {
 		const auto x = stx::lerp(a.x, b.x, t);
 		const auto y = stx::lerp(a.y, b.y, t);
@@ -49,20 +50,22 @@ namespace stx {
 
 
 
-	auto lerp(const color_rgb_no_alpha auto & c1, const color_rgb_no_alpha auto & c2,	auto t) {
-		const auto r = static_cast<std::uint8_t>(std::lerp(c1.r, c2.r, t));
-		const auto g = static_cast<std::uint8_t>(std::lerp(c1.g, c2.g, t));
-		const auto b = static_cast<std::uint8_t>(std::lerp(c1.b, c2.b, t));
+	template<color_rgb_no_alpha Color, std::floating_point T>
+	auto lerp(const Color & c1, const Color & c2, T t) {
+		const auto r = stx::lerp(c1.r, c2.r, t);
+		const auto g = stx::lerp(c1.g, c2.g, t);
+		const auto b = stx::lerp(c1.b, c2.b, t);
 		return decltype(c1){r, g, b};
 	}
 
 
 
-	auto lerp(const color_rgba auto & c1, const color_rgba auto & c2, auto t) {
-		const auto r = static_cast<std::uint8_t>(std::lerp(c1.r, c2.r, t));
-		const auto g = static_cast<std::uint8_t>(std::lerp(c1.g, c2.g, t));
-		const auto b = static_cast<std::uint8_t>(std::lerp(c1.b, c2.b, t));
-		const auto a = static_cast<std::uint8_t>(std::lerp(c1.a, c2.a, t));
+	template<color_rgba Color, std::floating_point T>
+	auto lerp(const Color & c1, const Color & c2, T t) {
+		const auto r = stx::lerp(c1.r, c2.r, t);
+		const auto g = stx::lerp(c1.g, c2.g, t);
+		const auto b = stx::lerp(c1.b, c2.b, t);
+		const auto a = stx::lerp(c1.a, c2.a, t);
 		return decltype(c1){r, g, b, a};
 	}
 
