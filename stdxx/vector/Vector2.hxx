11d8a2d898e65d93b4a1d7fbd64267615bb8953e
Jene Litsch <litsch.jene@gmail.com>
2023-09-13 18:38:44 +0200

    Feature: Added new stx::vector specializations

--- stdxx/vector/Vector2.hxx
+++ stdxx/vector/Vector2.hxx
@@ -17,6 +17,16 @@ namespace stx {
 	using vector2u  = vector2<unsigned>; 
 	using vector2ul = vector2<unsigned long>; 
 
+	using vector2i8 = vector2<std::int8_t>; 
+	using vector2i16 = vector2<std::int16_t>; 
+	using vector2i32 = vector2<std::int32_t>; 
+	using vector2i64 = vector2<std::int64_t>; 
+
+	using vector2u8 = vector2<std::uint8_t>; 
+	using vector2u16 = vector2<std::uint16_t>; 
+	using vector2u32 = vector2<std::uint32_t>; 
+	using vector2u64 = vector2<std::uint64_t>; 
+
 	template<typename Arithmetic> 
 	using size2   = vector2<Arithmetic, vectorFlavor::SIZE>;
 	using size2f  = size2<float>;
@@ -25,6 +35,16 @@ namespace stx {
 	using size2l  = size2<long>;
 	using size2u  = size2<unsigned>; 
 	using size2ul = size2<unsigned long>;
+
+	using size2i8 = size2<std::int8_t>; 
+	using size2i16 = size2<std::int16_t>; 
+	using size2i32 = size2<std::int32_t>; 
+	using size2i64 = size2<std::int64_t>; 
+
+	using size2u8 = size2<std::uint8_t>; 
+	using size2u16 = size2<std::uint16_t>; 
+	using size2u32 = size2<std::uint32_t>; 
+	using size2u64 = size2<std::uint64_t>; 
 
 
 	template<typename Arithmetic> 
@@ -36,6 +56,15 @@ namespace stx {
 	using position2u  = position2<unsigned>; 
 	using position2ul = position2<unsigned long>;
 
+	using position2i8 = position2<std::int8_t>; 
+	using position2i16 = position2<std::int16_t>; 
+	using position2i32 = position2<std::int32_t>; 
+	using position2i64 = position2<std::int64_t>; 
+
+	using position2u8 = position2<std::uint8_t>; 
+	using position2u16 = position2<std::uint16_t>; 
+	using position2u32 = position2<std::uint32_t>; 
+	using position2u64 = position2<std::uint64_t>; 
 
 
 	// Unary
